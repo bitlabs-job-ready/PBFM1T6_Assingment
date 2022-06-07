c5c89347f037a27b68a77b51f8e83e542b05ab17
bitLabs <75418136+bitlabs-job-ready@users.noreply.github.com>
2022-06-07 11:06:18 +0530

    Add files via upload

---
+++ PBFM1T6_Assingment2.py
@@ -0,0 +1,10 @@
+"""
+Write a Program For each word in words, add ‘d’ to the end of the word if the word ends in “e” to make it past tense.
+Otherwise, add ‘ed’ to make it past tense. Save these past tense words to a list called past_tense.
+
+Input:
+words = ["adopt", "bake", "beam", "confide", "grill", "plant", "time", "wave", "wish"]
+
+Output:
+['adopted', 'baked', 'beamed', 'confided', 'grilled', 'planted', 'timed', 'waved', 'wished']
+"""
---
+++ PBFM1T6_Assingment3.py
@@ -0,0 +1,17 @@
+"""
+write a program to generate all sentences where subject is in ["I", "You"] and verb is in ["Play", "Love"] and the object is in ["Hockey","Football"].
+
+Expected Output:
+
+I Play Hockey.
+I Play Football.
+I Love Hockey.
+I Love Football.
+You Play Hockey.
+You Play Football.
+You Love Hockey.
+You Love Football.
+
+Hints:
+Use list[index] notation to get a element from a list.
+"""
